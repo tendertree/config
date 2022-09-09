782c66547f5e30d6b914daa52eabdfafa292507d
unknown <tendertree@gmail.com>
2022-09-09 14:22:10 +0900

    remove some needless comment

--- nvim/lua/input.lua
+++ nvim/lua/input.lua
@@ -23,7 +23,8 @@ s('n', '<F6>', ':bnext!<CR>', { noremap = true })
 s('t', '<C-w>k', '<C-\\><C-n><C-w>k', { noremap = true }) --termial to buffer
 s('n', '<leader>w', '<C-w>w', { noremap = true })
 
---comment
+--
+s('n', 'gv', ':vsplit<CR>gd', { noremap = true })
 
 --lsp saga
 s('n', '<leader>hd', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
@@ -52,20 +53,6 @@ end, { silent = true })
 local lspconfig = require 'lspconfig'
 local on_attach = function(_, bufnr)
 	local opts = { noremap = true, silent = true }
-	--vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>g', '<cmd>lua Lspsaga hover_doc<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	--  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
-		'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	--vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so',
-		[[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
 	vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
