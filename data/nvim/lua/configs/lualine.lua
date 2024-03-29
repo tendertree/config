3d6d42f745e1284dc60486c6124c0267eb08789e
tendertree <tendertree@gmail.com>
2024-03-29 20:45:37 +0900

    fix: remove rust.nvim

--- data/nvim/lua/configs/lualine.lua
+++ data/nvim/lua/configs/lualine.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
 				symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
 			},
 		},
-		lualine_c = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'branch', 'diff', 'filename' },
 		lualine_x = { 'encoding', 'fileformat', 'filetype' },
 		lualine_y = { 'progress', 'getWords' },
 		lualine_z = { 'location', {
