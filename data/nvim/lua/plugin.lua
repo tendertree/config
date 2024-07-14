c0ef2fa1aa0991b300c96af774e39956f7de21fd
tendertree <tendertree@gmail.com>
2024-07-15 02:30:10 +0900

    feat: add some snippets

--- data/nvim/lua/plugin.lua
+++ data/nvim/lua/plugin.lua
@@ -518,6 +518,20 @@ require('lazy').setup({
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
+		},
+		{
+			"kylechui/nvim-surround",
+			version = "*", -- Use for stability; omit to use `main` branch for the latest features
+			event = "VeryLazy",
+			config = function()
+				require("nvim-surround").setup({
+					-- Configuration here, or leave empty to use defaults
+					keymaps = {
+						visual = "'",
+						visual_line = "'g",
+					}
+				})
+			end
 		}
 	}
 }, { defaults = { lazy = true } })
