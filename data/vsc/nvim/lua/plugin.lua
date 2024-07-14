584beb53d0ea2b72b087f9c5b7352b3491576044
tendertree <tendertree@gmail.com>
2024-07-15 01:51:42 +0900

    feat: add surround plugin

--- data/vsc/nvim/lua/plugin.lua
+++ data/vsc/nvim/lua/plugin.lua
@@ -16,6 +16,20 @@ require('lazy').setup({
 		"tendertree/nforcolemak-dh",
 	},
 	'savq/melange-nvim',
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+				keymaps = {
+					visual = "'",
+					visual_line = "'g",
+				}
+			})
+		end
+	}
 
 }
 )
