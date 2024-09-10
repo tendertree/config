ae63531d08c37ba55d0e7f47914c409323903491
tendertree <tendertree@gmail.com>
2024-09-10 23:50:05 +0900

    fix: organize nvim in the vsc folder

--- data/vsc/nvim/lua/input.lua
+++ data/vsc/nvim/lua/input.lua
@@ -26,25 +26,3 @@ s('n', ',h', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 local s = vim.api.nvim_set_keymap
-
-require("nforcolemak-dh")
-
-
-
-s('n', ':', '0p', { noremap = true })
-s('v', ':', '0p', { noremap = true })
-s('n', ';', ':', { noremap = true })
-s('v', ';', ':', { noremap = true })
-s('v', 'dd', '"_dd', { noremap = true })
-
-
-
-s('n', 'ss', '"_dd', { noremap = true, silent = true })
-s('n', '<F5>', ':undo<CR>', { noremap = true })
-s('n', '<leader>hv', '<Cmd>call VSCodeNotify("editor.action.revealDefinition")<CR>', { noremap = true, silent = true })
-s('n', '<leader>go', '<Cmd>call VSCodeNotify("bookmarks.listFromAllFiles")<CR>', { noremap = true, silent = true })
-s('n', '<leader>gg', '<Cmd>call VSCodeNotify("bookmarks.toggle")<CR>', { noremap = true, silent = true })
-s('n', 'gc', '<Cmd>call VSCodeNotify("editor.action.codeAction")<CR>', { noremap = true, silent = true })
-s('n', 'gr', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { noremap = true, silent = true })
-s('n', '<leader>q', '<Cmd>call VSCodeNotify("workbench.view.search")<CR>', { noremap = true, silent = true })
-s('n', ',h', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>', { noremap = true, silent = true })
